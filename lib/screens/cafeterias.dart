9c4c466c41ae08eef8fb55a9757480ffed475e5a
Michael Equi <equim20@student.jhs.net>
2020-08-19 10:48:05 -0700

    ignore google plist and added list view for cafeterias

--- lib/screens/cafeterias.dart
+++ lib/screens/cafeterias.dart
@@ -3,29 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key key}) : super(key: key);
   final String title = "Hello";
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     FirebaseUser user = Provider.of<FirebaseUser>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Cafeteria Page"),
         actions: <Widget>[
           IconButton(
             icon: Icon(FontAwesomeIcons.userCircle),
@@ -34,22 +21,35 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-                '${user?.displayName ?? "Guest"} has pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Container(
+          padding: EdgeInsets.only(top: 10),
+          child: ListView(
+            children: <Widget>[cafeteria(), cafeteria()],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => null,
         tooltip: 'Increment',
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class cafeteria extends StatelessWidget {
+  const cafeteria({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: FlatButton(
+        child: Text("Bowles dinning hall",
+            style: Theme.of(context).textTheme.headline4),
+        color: Colors.grey,
+        onPressed: () => null,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       ),
     );
   }
